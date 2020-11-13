147a9f4b35d6ad486aa7761a9898608ae34986d3
Bikossor <andre.lichtenthaeler@gmail.com>
2020-11-13 23:48:04 +0100

    #1 - SequenceOf combinator returns better results

--- src/combinators/sequenceOf.ts
+++ src/combinators/sequenceOf.ts
@@ -1,10 +1,15 @@
 export const sequenceOf = (parsers: Array<any>) => (input: string) => {
-  const results = [];
+  const results: Array<{ match: string, nextInput: string }> = [];
   let lastInput = input;
 
   parsers.forEach((parser) => {
     if (lastInput !== undefined) {
-      lastInput = parser(lastInput).nextInput;
+      const parsed = parser(lastInput);
+      lastInput = parsed.nextInput;
+      results.push({
+        match: parsed.match,
+        nextInput: parsed.nextInput
+      });
     }
   });
 
