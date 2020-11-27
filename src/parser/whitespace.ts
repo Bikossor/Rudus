ad28f072611893c5dbc35bcafcfb01e772ec6ae9
Bikossor <andre.lichtenthaeler@gmail.com>
2020-11-27 16:42:02 +0100

    #7 - Use destructuring assignments

--- src/parser/whitespace.ts
+++ src/parser/whitespace.ts
@@ -3,15 +3,15 @@ import { ParserState, updateParserError, updateParserState } from "../ParserStat
 
 export const whitespace = () => new Parser((state: ParserState): ParserState => {
   const regexWhitespace = /\s+/;
-  const execArr = regexWhitespace.exec(state.input.slice(state.offset));
+  const [fullMatch] = regexWhitespace.exec(state.input.slice(state.offset));
 
-  if (execArr === null) {
+  if (fullMatch === null) {
     return updateParserError(state, `Failed to match ${regexWhitespace}`);
   }
 
   return updateParserState(
     state,
-    state.offset + execArr[0].length,
-    execArr[0]
+    state.offset + fullMatch.length,
+    fullMatch
   );
 });
