966d118e07fe0d0e426f354803bb676068d3af30
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2022-03-15 23:21:54 +0100

    feat: add `endOfLine` parser (#116)
    * feat: add `endOfLine` parser
    
    #63
    
    * test: add `endOfLine` parser tests
    
    #63
    
    * chore: add export for endOfLine parser
    
    #63
    
    * fix: bad usage of regex (JS-D007)
    
    #63

---
+++ src/parser/endOfLine.ts
@@ -0,0 +1,20 @@
+import { Parser } from "../Parser";
+import { ParserState, updateParserError, updateParserState } from "../ParserState";
+
+/**
+ * Tries to match an end of line.
+ */
+export const endOfLine = () =>
+  new Parser((state: ParserState): ParserState => {
+    const endOfLineRegex = new RegExp(/(\r\n|\r|\n)/);
+    const [fullMatch] = endOfLineRegex.exec(state.input.slice(state.offset)) || [null];
+
+    if (fullMatch === null) {
+      return updateParserError(
+        state,
+        `Failed to match end of line at offset ${state.offset.toString()}`,
+      );
+    }
+
+    return updateParserState(state, state.offset + fullMatch.length, "<EOL>");
+  });
