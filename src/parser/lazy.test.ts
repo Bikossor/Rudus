9d1cf5d1c42bac5d35fd0cd767a951a41d304afb
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2022-03-21 23:49:14 +0100

    test: introduce `lazy` tests
    #119

---
+++ src/parser/lazy.test.ts
@@ -0,0 +1,24 @@
+import { anyOf, many, separatedBy } from "../combinators";
+import { lazy, regex, string } from "../parser/index";
+import { ParserState } from "../ParserState";
+
+describe("lazy", () => {
+  test("Parse a matchable input", () => {
+    const value = lazy(() => anyOf([numbers, strings, matchArray]));
+
+    const commaSeparatedValue = separatedBy(value, string(","));
+
+    const numbers = regex(/[0-9]+/);
+    const strings = regex(/[A-Z]+/);
+    const matchArray = many(commaSeparatedValue);
+
+    const result = matchArray.run("12,34,HELLO,");
+
+    expect(result).toStrictEqual<ParserState>({
+      input: "12,34,HELLO,",
+      isError: false,
+      offset: 12,
+      result: ["12", "34", "HELLO"],
+    });
+  });
+});
