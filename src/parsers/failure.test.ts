69b2576027dc755c98bfe4c4949dc66ee0d5ab58
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2022-08-03 22:20:27 +0200

    test: introduce `failure` tests
    #159

---
+++ src/parsers/failure.test.ts
@@ -0,0 +1,45 @@
+import { ParserState } from "../ParserState";
+import { failure } from "../parsers";
+
+describe("failure", () => {
+  it("should not override any previous error message", () => {
+    const parser = failure("Something failed!");
+    const previousState: ParserState = {
+      input: "World",
+      isError: true,
+      offset: 0,
+      result: null,
+      errorMessage: "Something previously failed!",
+    };
+
+    const result = parser.transformState(previousState);
+
+    expect(result).toStrictEqual<ParserState>({
+      input: "World",
+      isError: true,
+      errorMessage: "Something previously failed!",
+      offset: 0,
+      result: null,
+    });
+  });
+
+  it("should return the given error message when previously nothing failed", () => {
+    const parser = failure("Something failed!");
+    const previousState: ParserState = {
+      input: "Hello ",
+      isError: false,
+      offset: 6,
+      result: ["Hello", " "],
+    };
+
+    const result = parser.transformState(previousState);
+
+    expect(result).toStrictEqual<ParserState>({
+      input: "Hello ",
+      isError: true,
+      errorMessage: "Something failed!",
+      offset: 6,
+      result: ["Hello", " "],
+    });
+  });
+});
