534a55524afe9a6bcef7ef12868cc0117801a57b
André Lichtenthäler <andre.lichtenthaeler@gmail.com>
2022-08-13 22:31:54 +0200

    docs: add tsdoc `@see`
    #166

--- src/parsers/failure.ts
+++ src/parsers/failure.ts
@@ -5,6 +5,7 @@ import { updateParserError } from "../ParserState";
  * Always returns a failing parser with the given `errorMessage`. Typically used inside a contextual parser.
  * @param errorMessage
  * @returns
+ * @see https://rudus.pages.dev/docs/api/parsers/failure
  */
 export const failure = (errorMessage: string) =>
   new Parser(state => {
