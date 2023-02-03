ff50378ec8718556f063122bd1a4f48734a756fa
Bikossor <andre.lichtenthaeler@gmail.com>
2023-02-03 23:34:33 +0100

    test: add test for `Parser.ts`
    #211

---
+++ src/Parser.test.ts
@@ -0,0 +1,18 @@
+import { many1 } from "./combinators";
+import { string } from "./parsers";
+import { ParserState } from "./ParserState";
+
+describe("Parser", () => {
+  it("should map a single parser as expected", () => {
+    const parser = many1(string("Hello")).map(state => [state.result, "World"]);
+
+    const result = parser.run("HelloHello");
+
+    expect(result).toStrictEqual<ParserState>({
+      input: "HelloHello",
+      isError: false,
+      offset: 10,
+      result: [["Hello", "Hello"], "World"],
+    });
+  });
+});
