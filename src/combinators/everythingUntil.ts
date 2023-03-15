fabd3a508eafa8a42ed4aa161f2038d447f27da6
Bikossor <andre.lichtenthaeler@gmail.com>
2023-03-15 22:35:12 +0100

    feat: update everythingUntil combinator

--- src/combinators/everythingUntil.ts
+++ src/combinators/everythingUntil.ts
@@ -5,22 +5,23 @@ import { updateParserError, updateParserResult } from "../ParserState";
  * Tries to match a given `inner` surrounded by a given `outerLeft` and `outerRight`. The `outerRight` parser is optional and defaults to `outerLeft`.
  * @see https://rudus.pages.dev/docs/api/combinators/everythingUntil
  */
-export const everythingUntil = (separator: Parser) => (value: Parser) =>
-  new Parser(state => {
-    if (state.isError) return state;
+export const everythingUntil =
+  (separator: Parser) =>
+  (value: Parser, name = "everythingUntil") =>
+    new Parser(state => {
+      if (state.isError) return state;
 
-    const out = value.transformState(state);
+      const out = value.transformState(state);
 
-    if (out.isError)
-      return updateParserError(out, `everythingUntil: ${out.errorMessage}`);
+      if (out.isError) return updateParserError(out, `${name}: ${out.errorMessage}`);
 
-    const outSeperator = separator.transformState(out);
+      const outSeperator = separator.transformState(out);
 
-    if (outSeperator.isError)
-      return updateParserError(
-        outSeperator,
-        `everythingUntil: Failed to match separator at offset ${outSeperator.offset}`,
-      );
+      if (outSeperator.isError)
+        return updateParserError(
+          outSeperator,
+          `${name}: failed to match separator at offset ${outSeperator.offset}`,
+        );
 
-    return updateParserResult(outSeperator, out.result);
-  });
+      return updateParserResult(outSeperator, out.result);
+    }, name);
